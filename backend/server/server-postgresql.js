a299aafe5ba5cefff7572acf241c132dbfedd874
Aman kumar <aman9693kumar@gmail.com>
2024-10-09 14:17:20 +0530

    vercel

--- backend/server/server-postgresql.js
+++ backend/server/server-postgresql.js
@@ -105,6 +105,8 @@ app.get("/", (req, res) => {
     res.send("success");
 });
 
-app.listen(3000, () => {
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
     console.log("app is running on port 3000");
 })
